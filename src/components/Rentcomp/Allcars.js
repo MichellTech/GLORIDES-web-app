a5a3c57ca180166ec2da6c089173726bb73c07fe
Okwu chiedozie Michell <okwuchiedozie@gmail.com>
2023-10-06 12:34:45 +0100

    done

--- src/components/Rentcomp/Allcars.js
+++ src/components/Rentcomp/Allcars.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { BiSolidCarGarage } from 'react-icons/bi'
-import { LuFuel } from 'react-icons/lu'
+import { LuFuel, LuUser } from 'react-icons/lu'
 import { GiGearStickPattern, GiCarSeat, GiRoundStar } from 'react-icons/gi'
+import { AiOutlineHeart, AiFillHeart, AiFillStar } from 'react-icons/ai'
 import { TbClockSearch } from 'react-icons/tb'
 import { MdOutlineFilterAlt } from 'react-icons/md'
 import { useSelector, useDispatch } from 'react-redux'
@@ -10,6 +11,7 @@ import { openFilter } from '@/features/rental/filterSlice'
 import { cars } from '../../utilis/Cardata'
 import { useRouter } from 'next/router'
 function Allcars() {
+  const [saved, setSaved] = useState(false)
   const { isFiltering } = useSelector((store) => store.rental)
   const router = useRouter()
   const dispatch = useDispatch()
@@ -38,14 +40,7 @@ function Allcars() {
           return (
             <div key={item.id}>
               {/* car 1 */}
-              <div
-                onClick={() => {
-                  router.push({
-                    pathname: `/rentacar/${item.id}`,
-                  })
-                }}
-                className='bg-white shadow-md h-[20rem] rounded-xl px-3 py-2 space-y-4 max-w-xs cursor-pointer'
-              >
+              <div className='bg-white shadow-lg h-[22rem] rounded-xl  pb-2 space-y-4 max-w-xs  relative w-full '>
                 {/* image */}
                 <div className='   relative '>
                   <Image
@@ -53,51 +48,86 @@ function Allcars() {
                     alt='footer'
                     width={1000}
                     height={1000}
-                    className='object-cover w-full h-40 rounded-lg '
+                    className='object-cover w-full h-40 rounded-tl-lg rounded-tr-lg rounded-br-none  rounded-bl-none '
                   />
                 </div>
 
                 {/*text */}
-                <div className='px-2 w-full '>
-                  {/* title */}
-                  <div className='flex items-center justify-between border-b-2 pb-3 '>
-                    <h1 className='font-bold'>{item.carname}</h1>
-                    <h1 className='font-bold text-babypurple'>
-                      ${item.cost} /{' '}
-                      <span className='text-sm text-black font-normal'>hr</span>
+                <div className='px-4 w-full '>
+                  {/* first part */}
+                  <div className='space-y-2 border-b-2 pb-3'>
+                    {/* <div className='border border-babygrey rounded-md px-2 py-1 w-max'>
+                      <h1 className='text-xs'>2020</h1>
+                    </div> */}
+                    {/* carname */}
+                    <h1 className='font-bold text-sm line-clamp-1'>
+                      {item.carname}
                     </h1>
+                    {/* owner and cost */}
+                    <div className='flex items-center justify-between  '>
+                      <div className='flex justify-center items-center gap-2'>
+                        <LuUser className='text-sm' />
+                        <h1 className='text-xs'>Olamide Oluwale</h1>
+                      </div>
+                      <h1 className='font-bold text-sm text-babypurple'>
+                        ${item.cost} /{' '}
+                        <span className='text-sm text-babypurple font-normal'>
+                          day
+                        </span>
+                      </h1>
+                    </div>
                   </div>
-                  {/* params */}
-                  <div className='pt-3 grid grid-cols-3 gap-x-1 gap-y-6 justify-center items-center mx-auto'>
-                    {/* one */}
-                    <div className='flex items-center gap-2'>
-                      <BiSolidCarGarage className='text-base' />
-                      <h1 className='text-[0.6rem]'> 4 Doors</h1>
+                  {/* second */}
+                  <div className='pt-6 space-y-4'>
+                    {/* params */}
+                    <div className=' grid grid-cols-3 gap-x-1 gap-y-6 justify-center items-center mx-auto'>
+                      {/* two */}
+                      <div className='flex items-center gap-2'>
+                        <LuFuel className='text-base' />
+                        <h1 className='text-[0.6rem]'>Petrol</h1>
+                      </div>
+                      {/* three */}
+                      <div className='flex items-center gap-2'>
+                        <GiGearStickPattern className='text-base' />
+                        <h1 className='text-[0.6rem]'>Manual</h1>
+                      </div>
+
+                      {/* six */}
+                      <div className='flex items-center gap-2'>
+                        <TbClockSearch className='text-base' />
+                        <h1 className='text-[0.6rem]'> 2.4k Miles</h1>
+                      </div>
                     </div>
-                    {/* two */}
-                    <div className='flex items-center gap-2'>
-                      <LuFuel className='text-base' />
-                      <h1 className='text-[0.6rem]'> Diesel</h1>
+                    {/* button */}
+                    <button
+                      onClick={() => {
+                        router.push({
+                          pathname: `/rentacar/${item.id}`,
+                        })
+                      }}
+                      className='bg-babypurple px-2 py-2 w-full text-xs text-white rounded-md cursor-pointer hover:shadow-lg'
+                    >
+                      Rent Now
+                    </button>
+                  </div>
+                </div>
+                {/* buttons top */}
+                <div className=' absolute -top-2 left-2 right-2 '>
+                  <div className='flex justify-between items-center gap-2 mx-auto w-full'>
+                    {/* ratings */}
+                    <div className='flex justify-center items-center gap-1 rounded-md bg-white px-2 py-1'>
+                      <h1 className='text-xs'>5.0</h1>
+                      <AiFillStar className='text-xs text-yellow-500' />
                     </div>
-                    {/* three */}
-                    <div className='flex items-center gap-2'>
-                      <GiGearStickPattern className='text-base' />
-                      <h1 className='text-[0.6rem]'>Manual</h1>
-                    </div>
-                    {/* four */}
-                    <div className='flex items-center gap-2'>
-                      <GiCarSeat className='text-base' />
-                      <h1 className='text-[0.6rem]'> 4 Seater</h1>
-                    </div>
-                    {/* five */}
-                    <div className='flex items-center gap-2'>
-                      <GiRoundStar className='text-base' />
-                      <h1 className='text-[0.6rem]'> 4/5 Rating</h1>
-                    </div>
-                    {/* six */}
-                    <div className='flex items-center gap-2'>
-                      <TbClockSearch className='text-base' />
-                      <h1 className='text-[0.6rem]'> 2400 Miles</h1>
+                    {/* saved */}
+                    <div onClick={() => setSaved(!saved)} className=' '>
+                      <div className=' bg-white flex justify-center items-center rounded-md mx-auto cursor-pointer w-6 h-6'>
+                        {saved ? (
+                          <AiFillHeart className='text-sm  text-red-500' />
+                        ) : (
+                          <AiOutlineHeart className='text-sm    text-red-500 ' />
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
