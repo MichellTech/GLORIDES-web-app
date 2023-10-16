3659fc4a15ac2fc42538226ed930bff4e2101c7a
Okwu chiedozie Michell <okwuchiedozie@gmail.com>
2023-10-16 15:40:22 +0100

    fixed

---
+++ src/components/Profilecomp.js
@@ -0,0 +1,79 @@
+import React, { useEffect } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+function Profilecomp() {
+  const router = useRouter()
+
+  return (
+    <>
+      {/* small screen */}
+      <div className='relative example  overflow-y-auto w-max px-6 '>
+        <div className='flex space-x-10 text-sm '>
+          <Link
+            href='/Userprofile/view'
+            className={`${
+              router.pathname === '/Userprofile/view'
+                ? 'border-b-4  rounded-sm border-babypurple pb-3 font-bold text-babypurple '
+                : '  rounded-sm  pb-3 '
+            }`}
+          >
+            My Profile
+          </Link>
+          <Link
+            href='/Userprofile/edit'
+            className={`${
+              router.pathname === '/Userprofile/edit'
+                ? 'border-b-4  rounded-sm border-babypurple pb-3 font-bold  text-babypurple '
+                : '  rounded-sm  pb-3 '
+            }`}
+          >
+            Edit Profile
+          </Link>
+          <Link
+            href='/Userprofile/payment'
+            className={`${
+              router.pathname === '/Userprofile/payment'
+                ? 'border-b-4 font-bold   rounded-sm border-babypurple pb-3 text-babypurple '
+                : '  rounded-sm  pb-3 '
+            }`}
+          >
+            Payment Cards
+          </Link>
+          <Link
+            href='/Userprofile/password'
+            className={`${
+              router.pathname === '/Userprofile/password'
+                ? 'border-b-4 font-bold   rounded-sm border-babypurple pb-3 text-babypurple '
+                : '  rounded-sm  pb-3 '
+            }`}
+          >
+            Password
+          </Link>
+          <Link
+            href='/Userprofile/pin'
+            className={`${
+              router.pathname === '/Userprofile/pin'
+                ? 'border-b-4  font-bold  rounded-sm border-babypurple pb-3 text-babypurple '
+                : '  rounded-sm  pb-3 '
+            }`}
+          >
+            Pin
+          </Link>
+          <Link
+            href='/Userprofile/accounts'
+            className={`${
+              router.pathname === '/Userprofile/accounts'
+                ? 'border-b-4 font-bold  rounded-sm border-babypurple pb-3 text-babypurple '
+                : '  rounded-sm  pb-3 '
+            }`}
+          >
+            Accounts
+          </Link>
+        </div>
+        <div className='absolute left-0 w-full right-0 bottom-0 border-b  '></div>
+      </div>
+    </>
+  )
+}
+
+export default Profilecomp
